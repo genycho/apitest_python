2b3bdc78f791c908442cf55d8dea0658093fc606
TomNJerry <genycho@naver.com>
2021-03-05 14:15:23 +0900

    직접관련이 없는 파일들은 삭제

--- tests/common/exceptions.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-class APITestException(Exception):
-    def __init__(self, message):
-        self.value = message
-
-    def __str__(self):
-        return self.value
